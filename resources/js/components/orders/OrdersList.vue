65a6aade640d86fcc2b268a86e353a5beb6e41f2
maksakoviliya <maksak_il@mail.ru>
2022-03-25 18:53:21 +0300

    Update OrdersList.vue

--- resources/js/components/orders/OrdersList.vue
+++ resources/js/components/orders/OrdersList.vue
@@ -64,7 +64,7 @@
           <tr v-for="order in orders" :key="order.id">
             <td class="px-3 py-2 whitespace-nowrap">
               <input
-                  v-if="!order.act_id && order.status !== 'processing'"
+                  v-if="!order.act_id"
                   class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                   type="checkbox"
                   :value="order.id"
